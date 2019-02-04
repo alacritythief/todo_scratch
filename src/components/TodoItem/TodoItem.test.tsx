e2f3735e07a87378427fe96fc1462864ebb34209
Andy Wong <awong@cogolabs.com>
2019-02-04 13:06:28 -0500

    made a better test

--- src/components/TodoItem/TodoItem.test.tsx
+++ src/components/TodoItem/TodoItem.test.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoItem from './TodoItem';
 
+const textArray: string[] = [
+  'Buy Eggs',
+  'Buy Milk',
+  'Buy Bread',
+  'Make French Toast',
+  'Walk the dog',
+  'Pet my cat',
+]
+
+function randomText () {
+  let max = textArray.length - 1;
+  return textArray[Math.floor(Math.random() * Math.floor(max))]
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<TodoItem initItem="Buy Eggs"/>, div);
+  ReactDOM.render(<TodoItem initItem={ randomText() }/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
