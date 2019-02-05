e36c0f44819b539d36b336bb02f84f0cda58364b
Andy Wong <awong@cogolabs.com>
2019-02-04 23:46:09 -0500

    fix test

--- src/components/TodoInput/TodoInput.test.tsx
+++ src/components/TodoInput/TodoInput.test.tsx
@@ -4,6 +4,10 @@ import TodoInput from './TodoInput';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<TodoInput/>, div);
+  ReactDOM.render(<TodoInput callback={
+    () => {
+      return;
+    }
+  }/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
